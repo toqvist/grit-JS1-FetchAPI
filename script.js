93a511248dd13e31912eb81bdef85a81bb4c897c
toqvist <tobias-ahlqvist@outlook.com>
2022-01-26 14:56:07 +0100

    Finished assignment

--- script.js
+++ script.js
@@ -1,34 +1,48 @@
-//Lägg till din egna KEY
-const KEY = 'b4e997214e2f87d6f3582422b364dab9';
-let searchText = 'frog';
 
-//Vi söker endast på 1foto per sida och 1 sida
-const url = `https://www.flickr.com/services/rest/?api_key=${KEY}&method=flickr.photos.search&text=${searchText}&format=json&nojsoncallback=1&per_page=1&page=1`;
+const input = document.querySelector("input");
 
-fetch(url).then(
-    function(response){
-        console.log(response);
-        if(response.status>=200 && response.status<300){
-            return response.json();
+const button = document.querySelector("button");
+button.addEventListener('click', imageSearch);
+
+
+function imageSearch () {
+
+    clearImagesAndErrors();
+
+    let searchText = input.value;
+
+    const KEY = 'b4e997214e2f87d6f3582422b364dab9';
+
+    const url = `https://www.flickr.com/services/rest/?api_key=${KEY}&method=flickr.photos.search&text=${searchText}&format=json&nojsoncallback=1&per_page=1&page=1`;
+    console.log(url)
+
+    fetch(url).then(
+        function(response){
+            console.log(response);
+            if(response.status>=200 && response.status<300){
+                return response.json();
+            }
+            else{
+                displayError(response.status);
+                throw 'Error: ' + response.status;
+
+            }
         }
-        else{
-            throw 'Something went wrong. :(';
+    ).then(
+        function(data){
+            console.log(data);
+            //Vi hämtar första bilden
+            buildImageUrl(data.photos.photo[0]);
         }
-    }
-).then(
-    function(data){
-        console.log(data);
-        //Vi hämtar första bilden
-        getImageUrl(data.photos.photo[0]);
-    }
-).catch(
-    function(error){
-        console.log(error);
-    }
-);
-
+    ).catch(
+        function(error){
+            console.log(error);
+            displayError(error.name)
+        }
+    );
+}
 //här ska vi pussla ihop bild-urlen
-function getImageUrl(photoObject){
+function buildImageUrl(photoObject){
     let photo = photoObject;
     let size = 'm';
 
@@ -39,9 +53,30 @@ function getImageUrl(photoObject){
 }
 
 //för att visa bilden
-function displayImg(url){
+function displayImg(imgUrl){
     let img = document.createElement('img');
-    img.src = url;
+    img.src = imgUrl;
 
     document.body.appendChild(img);
+}
+
+function displayError(errorCode) {
+    let p = document.createElement('p');
+    p.innerText = `${errorCode} occured: Search query yielded no results`;
+    document.body.appendChild(p);
+}
+
+function clearImagesAndErrors () {
+  
+    const images = document.querySelectorAll('img');
+
+    for(const img of images){
+        img.remove();
+    }
+
+    const messages = document.querySelectorAll('p');
+
+    for(const message of messages){
+        message.remove();
+    }
 }
